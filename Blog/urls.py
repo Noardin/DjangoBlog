531db7e8f0bb86e2a9f592cc56c19e25fb6b865a
Noardin <triksiand@gmail.com>
2020-09-23 14:02:48 +0200

    nav, registration

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     re_path(r'^(?P<article_id>[0-9]{1,4})/(?P<expand_comments>true|false)', views.articles, name="article_comment"),
     path('', views.articles, name="articles"),
     path('addcomment', views.add_comment, name="add_comment"),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signUp',views.sign_up, name="signUp")
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Articles, Comments
+from django.contrib.auth import login, authenticate
+from .models import Articles, Comments, Profile
 from django.contrib.auth.forms import UserCreationForm
 from .blogForms import ProfileForm
 
-def articles(request, article_id =0, expand_comments='false'):
 
+def articles(request, article_id =0, expand_comments='false'):
     articles_list= Articles.objects.all()
     print(expand_comments)
     if(article_id == 0):
@@ -16,6 +17,7 @@ def articles(request, article_id =0, expand_comments='false'):
     context = {'article': article, 'expand_comments': expand_comments, 'comments': comment_list}
     return render(request, 'blog/article_detail.html', context= context)
 
+
 def add_comment(request):
     if request.method == "POST":
         Article = Articles.objects.get(pk=request.POST['article_id'])
@@ -23,14 +25,23 @@ def add_comment(request):
         Comment.save()
     return redirect(articles,article_id=request.POST["article_id"], expand_comments='true')
 
+
 def sign_up(request):
     if request.method == "POST":
-        userCreationForm = UserCreationForm(request.POST["userCreationForm"])
-        profileForm = ProfileForm(request.POST["profileForm"])
-        context = {
-            "userCreationForm": userCreationForm,
-            "profileForm":profileForm
-        }
-        render(request, 'registration/signUp.html', context)
+        userCreationForm = UserCreationForm(request.POST)
+        profileForm = ProfileForm(request.POST)
+        if userCreationForm.is_valid() and profileForm.is_valid():
+            new_user = userCreationForm.save()
+            user_profile = Profile.objects.get(user_id= new_user.id)
+            user_profile.profile_image = profileForm['profile_image']
+            username = userCreationForm.cleaned_data.get('username')
+            raw_password = userCreationForm.cleaned_data.get('password1')
+            user = authenticate(username= username, password=raw_password)
+            login(request, user)
+            return redirect('articles')
     else:
-        render(request, 'registration/signUp.html')
+        print('nonPost')
+        userCreationForm = UserCreationForm()
+        profileForm = ProfileForm()
+
+    return render(request, 'registration/signUp.html', { "userCreationForm": userCreationForm, "profileForm": profileForm })
